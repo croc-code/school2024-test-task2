c4d270d87832cbf072c282f937b28bbce264849b
User <baydak.danila00@mail.ru>
2024-04-30 02:06:07 +0300

    ADD comments

--- src/main/java/ru/baydak/App.java
+++ src/main/java/ru/baydak/App.java
@@ -6,14 +6,12 @@ import java.io.File;
 import java.io.InputStream;
 import java.util.Objects;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
+        // Проверка на наличие аргумента path
         if(args.length < 1) {
             System.out.println("Usage: java Main <path_to_format.json>");
             System.exit(1);
